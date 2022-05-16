d3740dcb8fa0111988750e52e24f3e4f3f5c81db
ignitfury <105622649+ignitfury@users.noreply.github.com>
2022-05-16 15:44:45 +0530

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,54 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.lang.builder import Builder
+from kivy.uix.popup import Popup
+import smtplib
+from kivy.uix.floatlayout import FloatLayout
+Builder.load_file('test.kv')
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('prempatel2445@gmail.com', 'kpmcujdjxvdhdihj')
+    # server.login('prempatel2445@gmail.com', 'prempatel22!')
+    server.sendmail('prempatel2445@gmail.com', to, content)
+    server.close()
+
+class MyGrid(Widget):
+    email = ObjectProperty(None)
+    context = ObjectProperty(None)
+    def btn(self):
+        try:
+            to  = self.email.text
+            content = self.context.text
+            sendEmail(to, content)
+            print("Sended The Mail to", to)
+            self.email.text= ""
+            self.context.text= ""
+            s_pop()
+        except Exception as e:
+                print(e)
+                return Myapp()
+
+
+class Myapp(App):
+     def build(self):
+        return MyGrid()
+
+class p(FloatLayout):
+    pass
+def s_pop():
+    show = p()
+
+    popWindow = Popup(title="Mail Respond", content=show, size_hint=(None,None), size=(400,400))
+    popWindow.open()
+
+if __name__ == '__main__':
+     Myapp().run()
