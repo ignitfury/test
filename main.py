c6f0bc9bdb77c4f38b7c4d0f82cd1f83c6512655
ignitfury <105622649+ignitfury@users.noreply.github.com>
2022-05-16 14:12:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,64 +1,27 @@
-from cProfile import label
-from pydoc import text
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
-
-class ygrid(GridLayout):
-    def __init__(self, **kwargs):
-        super(ygrid, self).__init__(**kwargs)
-        self.cols = 1
-        
-        self.inside = GridLayout()
-        self.inside.cols = 2
-
-        self.inside.add_widget(Label(text="Enter Your Number: "))
-        self.numb= TextInput(multiline =False)
-        self.inside.add_widget(self.numb)
-
-        # self.inside.add_widget(Label(text="Email: "))
-        # self.email = TextInput(multiline =False)
-        # self.inside.add_widget(self.email)
-        
-        # self.inside.add_widget(Label(text="Skill: "))
-        # self.skill = TextInput(multiline =False)
-        # self.inside.add_widget(self.skill)
-        
-        self.submit = Button(text='Submit', font_size=40)
-        self.submit.bind(on_press = self.pressed)
-
-        self.add_widget(self.submit)
-
-        
-        self.add_widget(self.inside)
-
-    def pressed(self, instance):
-        # name = self.name.text
-        # email = self.email.text
-        # skill= self.skill.text
-        numb = str(self.numb.text)
-        # print("Name: ", name)
-        # print("Email:" , email)
-        # print("Skill: ", skill)
-
-        t = eval(numb)
-        print(t)
-        self.numb.text = ""
-        # self.name.text = ""
-        # self.email.text = ""
-        # self.skill.text = ""
-
-        
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
 
 
+class MyGrid(Widget):
+    equ = ObjectProperty(None)
+    def sum(self):
+        cal = self.equ.text
+        if cal:
+            try:
+                # Solve formula and display it in entry
+                # which is pointed at by display
+                self.equ.text = str(eval(cal))
+            except Exception:
+                self.equ.text = "Error"
 
 class Myapp(App):
-    def build(self):
-        return ygrid()
-
-
+     def build(self):
+        return MyGrid()
 if __name__ == '__main__':
-    Myapp().run()
+     Myapp().run()
