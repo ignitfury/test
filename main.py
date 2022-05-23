7c887e06794ee313f99b719401e1a6de71c361dd
ignitfury <105622649+ignitfury@users.noreply.github.com>
2022-05-23 11:06:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,53 +1,78 @@
-import kivy
 from kivy.app import App
-from kivy.uix.label import Label
+
+from kivy.core.window import Window
+
+from kivy.config import Config
+
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
-from kivy.uix.widget import Widget
-from kivy.properties import ObjectProperty
+from kivy.uix.label import Label
+from kivy.uix.button import Button 
 from kivy.uix.popup import Popup
-import smtplib
-from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.scrollview import ScrollView
 
+from kivy.properties import NumericProperty
 
-def sendEmail(to, content):
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.ehlo()
-    server.starttls()
-    server.login('prempatel2445@gmail.com', 'kpmcujdjxvdhdihj')
-    # server.login('prempatel2445@gmail.com', 'prempatel22!')
-    server.sendmail('prempatel2445@gmail.com', to, content)
-    server.close()
+class CalculatorWidget(GridLayout):
+    int_button_width = 120
+    int_button_height = 100
+    int_button_font_size = 40
 
-class MyGrid(Widget):
-    email = ObjectProperty(None)
-    context = ObjectProperty(None)
-    def btn(self):
-        try:
-            to  = self.email.text
-            content = self.context.text
-            sendEmail(to, content)
-            print("Sended The Mail to", to)
-            self.email.text= ""
-            self.context.text= ""
-            s_pop()
-        except Exception as e:
-                print(e)
-                return Myapp()
+    button_width = NumericProperty(int_button_width)
+    button_height = NumericProperty(int_button_height)
+    button_font_size = NumericProperty(int_button_font_size)
 
+    Window.size = (int_button_width * 2, 
+        int_button_height * 3)
 
-class Myapp(App):
-     def build(self):
-        return MyGrid()
+    def calc_error(self, error, calc_entry):
+        content = BoxLayout(orientation='vertical')
+        scrollview = ScrollView()
 
-class p(FloatLayout):
-    pass
-def s_pop():
-    show = p()
+        close_popup = Button(text='Close this popup')
+        error_message = Label(text=str(error))
 
-    popWindow = Popup(title="Mail Respond", content=show, size_hint=(None,None), size=(400,400))
-    popWindow.open()
+        scrollview.add_widget(error_message)
+        content.add_widget(scrollview)
+        content.add_widget(close_popup)
+
+        popup = Popup(title='An error occured',
+            content=content, size_hint=(.8, .8))
+
+        close_popup.bind(on_release=popup.dismiss)
+        popup.open()
+
+    def calculate(self, *args):
+        calc_entry = self.ids.calc_input.text 
+        if calc_entry != '':
+            if calc_entry[0] in '1234567890-+':
+                try:
+                    ans = str(eval(calc_entry))
+                    self.ids.calc_input.text = ans
+                except Exception as error:
+                    self.calc_error(error, calc_entry)
+                    pass
+
+    def delete(self, *args):
+        self.ids.calc_input.text = self.ids.calc_input.text[:-1]
+
+    def clear(self, *args):
+        self.ids.calc_input.text = ''
+
+    def switch(self, *args):
+        calc_entry = self.ids.calc_input.text
+        if calc_entry != '':
+            if calc_entry[0] in '+1234567890':
+                self.ids.calc_input.text = '-' + calc_entry
+            if calc_entry[0] == '-':
+                self.ids.calc_input.text = calc_entry[1:]
+
+class CalculatorApp(App):
+    def build(self):
+        Config.set('graphics', 'resizable', '0')
+        Config.write()
+        return CalculatorWidget()
 
 if __name__ == '__main__':
-     Myapp().run()
+    CalculatorApp().run()
